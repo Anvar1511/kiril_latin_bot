21ee8ee7f2595cd55589d9799e7d2507f586e217
Anvar1511 <34777153+Anvar1511@users.noreply.github.com>
2021-02-22 21:57:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# from aiogram import executor, Bot
+  # from aiogram import executor, Bot
 # from loader import dp
 # import middlewares, filters, handlers
 # from utils.notify_admins import on_startup_notify
@@ -10,8 +10,20 @@
 #
 # if __name__ == '__main__':
 #     executor.start_polling(dp, on_startup=on_startup)
-from aiogram import Bot
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
 
 bot = Bot(token="1039659888:AAGAzdArAUlCQ5l68DKeE2N89Yt8FCNcjhI")
+dp = Dispatcher(bot)
 
-bo
+
+
+@dp.message_handler()
+async def get_message(message: types.Message)
+    chat_id = nessage.chat.id
+    text = 'salom, men test botman'
+
+    awaid bot.Send_message(chat_id=, text=text)
+
+bot.get_updates()
+executor.start_polling(dp)
